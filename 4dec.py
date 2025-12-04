743abf6c9bfc738d494c54213ac27de337db1d46
Rashid241001 <rashidfazal2410@gmail.com>
2025-12-04 10:16:46 +0000

    First commit

---
+++ 4dec.py
@@ -0,0 +1,24 @@
+def add(x,y):
+    return x + y
+def sub(x,y):
+    return x - y
+def mul(x,y):
+    return x * y
+def div(x,y):
+    if y==0:
+        return "Error:cannot divide by zero"
+    return x / y
+
+def display_menu():
+ print("\n------Calculator menu------ ")
+
+print("Choose operaton + , - , * , /")
+print("1.  ADD + ")
+print("2. Subtract - ")
+print("3. Multiply + ")
+print("4. Division / ")
+print("5. Exit ")
+
+
+
+
